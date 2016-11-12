be3e939e09e1fe1d9d56c930fb8b91b9d49f8ba0
Louis Lai <hoangdung2506@gmail.com>
2016-11-12 22:28:51 +0200

    Add logo

--- wp-content/themes/twentyfourteen/header.php
+++ wp-content/themes/twentyfourteen/header.php
@@ -37,7 +37,10 @@
 
 	<header id="masthead" class="site-header" role="banner">
 		<div class="header-main">
-			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+			<h1 class="site-title">
+<!--                <a href="--><?php //echo esc_url( home_url( '/' ) ); ?><!--" rel="home">--><?php //bloginfo( 'name' ); ?>
+<!--                </a>-->
+            </h1>
 
 			<div class="search-toggle">
 				<a href="#search-container" class="screen-reader-text" aria-expanded="false" aria-controls="search-container"><?php _e( 'Search', 'twentyfourteen' ); ?></a>
